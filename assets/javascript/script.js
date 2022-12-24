4d76996f7b5a9c30b0badf4e849e8cb953530f66
Sam M <samdm@posteo.net>
2022-12-24 05:49:15 -0800

    Added movesets and random computer moves put output of battle into browser moved some functions around so it is easier to read created slots for users to chose moves added two moves per character

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,32 +3,76 @@
     var characterList = [
         {
             name: "Sly Cooper",
+            code: 'sly',
             health: 150,
-            attack: 8,
-            counter: 15,
-            defaultAttack: 8,
-            pic: "../unit-4-game/assets/images/sly.jpg"
+            pic: "../unit-4-game-master/assets/images/sly.jpg",
+            moveSet: [{
+                name:"Cane Wack",
+                attack:8, // damage done to opponent
+                defense:0, // damage deflected from self
+                attack_chance: 80, // chance to land a hit to opponent (attack success)
+                defense_chance: 0 // chance to defend self (defense success)
+            },{
+                name:"Parry",
+                attack:0,
+                defense:8,
+                attack_chance: 0,
+                defense_chance: 80
+            }]
         }, {
             name: "Clockwerk",
+            code: 'clk',
             health: 180,
-            attack: 7,
-            counter: 25,
-            defaultAttack: 7,
-            pic: "../unit-4-game/assets/images/Clockwerkmetal.png"
+            pic: "../unit-4-game-master/assets/images/Clockwerkmetal.png",
+            moveSet: [{
+                name:"Bird Breath",
+                attack:8,
+                defense:0,
+                attack_chance: 80,
+                defense_chance: 0
+            },{
+                name:"Wing Flutter",
+                attack:0,
+                defense:8,
+                attack_chance: 0,
+                defense_chance: 90
+            }]
         }, {
             name: "Murray",
+            code: 'mur',
             health: 120,
-            attack: 8,
-            counter: 20,
-            defaultAttack: 8,
-            pic: "../unit-4-game/assets/images/murray.jpg"
+            pic: "../unit-4-game-master/assets/images/murray.jpg",
+            moveSet: [{
+                name:"Sucka Punch",
+                attack:8,
+                defense:0,
+                attack_chance: 80,
+                defense_chance: 0
+            },{
+                name:"Parry",
+                attack:0,
+                defense:8,
+                attack_chance: 0,
+                defense_chance: 80
+            }]
         }, {
             name: "Dimitri Lousteau",
+            code: 'dim',
             health: 100,
-            attack: 14,
-            counter: 5,
-            defaultAttack: 14,
-            pic: "../unit-4-game/assets/images/dimitrilousteau.jpg"
+            pic: "../unit-4-game-master/assets/images/dimitrilousteau.jpg",
+            moveSet: [{
+                name:"Elbow Smack",
+                attack:8,
+                defense:0,
+                attack_chance: 80,
+                defense_chance: 0
+            },{
+                name:"Purple Charge",
+                attack:0,
+                defense:8,
+                attack_chance: 0,
+                defense_chance: 100
+            }]
         }
 
     ];
@@ -53,7 +97,9 @@
         playerPicked = false;
         computerPicked = false;
         audio.play().loop;
+        $("#actions").hide();
         $("#character-grid").hide();
+        $("#move-grid").hide();
         printCharacters(gameRoster);
         
     }
@@ -78,12 +124,14 @@
                 var charStats = $("<div>");
                 charStats
                     .addClass("character-stats")
-                    .html(`<h4>${characterInfo.name}</h4>HP: ${characterInfo.health} <br/>Attack: ${characterInfo.attack}<br/>Counter: ${characterInfo.counter}`)
+                    .html(`<h4>${characterInfo.name}</h4>HP: ${characterInfo.health} <br/>`)
                     .appendTo(character);
 
                 $("#character-list").append(character)
             })
     }
+
+    
 
     //add player to active game area
     function activatePlayer(playerInfo, playerPosition, playerStats, playerTitle) {
@@ -92,7 +140,7 @@
             "background-image": "url(" + playerInfo.pic + ")"
         })
         $(playerStats)
-            .html(`<h4>${playerInfo.name} - ${playerTitle}</h4>HP: ${playerInfo.health} <br/>Attack: ${playerInfo.attack}<br/>Counter: ${playerInfo.counter}`)
+            .html(`<h4>${playerInfo.name} - ${playerTitle}</h4>HP: ${playerInfo.health} <br/>`)
             .removeClass("defeated")
     }
 
@@ -109,9 +157,14 @@
                     ...gameRoster[playerID]
                 };
                 $("#character-grid").show();
-                gameRoster.splice(playerID, 1);
+                gameRoster.splice(playerID, 1); 
                 printCharacters(gameRoster);
                 activatePlayer(player, "#player", "#player-stats", "Attacker");
+                $("#move-grid").show();
+                $("#slot1").html(`${player.moveSet[0].name}`);
+                $("#slot2").html(`${player.moveSet[1].name}`);
+                $("#actions").show();
+                $("#actions").html(`Player chose ${player.name}<br>`);
             } else if (!computerPicked && gameRunning) {
                 computerPicked = true;
                 computer = {
@@ -121,18 +174,131 @@
                 gameRoster.splice(playerID, 1);
                 printCharacters(gameRoster);
                 activatePlayer(computer, "#computer", "#computer-stats", "Defender");
+                $("#actions").append(`Computer is ${computer.name}<br>`);
             }
             else {
                 alert("Both players are active");
             }
         });
+    
+    
+        function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+          }
 
-    function attack() {
-        //player attacks cpu
-        computer.health -= player.attack;
-        //player's attack increases
-        player.attack += player.defaultAttack
-        // check to see if defender has lost
+    function print_charachters(){
+        $("#player-stats").html(`<h4>${player.name} - Attack</h4>HP: ${player.health} <br/>`);
+        $("#computer-stats").html(`<h4>${computer.name} - Defender</h4>HP: ${computer.health} <br/>`);
+    }
+
+    function changeHealthAfterAttack(attacker, attackee){
+        attackee_health = attackee.health;
+        if (attacker.attack >= attackee.health){
+            attackee.health = 0;
+        } else {
+            attackee.health -= attacker.attack;
+        }
+    }
+
+
+    
+    function changeHealthAfterAttack(attacker, attackee){
+        attackee_health = attackee.health;
+        if (attacker.attack >= attackee.health){
+            attackee.health = 0;
+        } else {
+            attackee.health -= attacker.attack;
+        }
+    }
+
+    function changeHealth(character, damageDealt) {
+        attackee_health = character.health;
+        if (damageDealt >= character.health){
+            character.health = 0;
+        } else {
+            character.health -= damageDealt;
+        }
+    }
+    function attackValue(slotMove){
+        var ranNum = randomNumber(1, 100); // 1 to 100 inclusive (0 chance items never succeed!)
+        if (slotMove.attack_chance >= ranNum){
+            return slotMove.attack;
+        } else {
+            return 0;
+        }
+    }
+    function defenseValue(slotMove){
+        var ranNum = randomNumber(1,100); // 1 to 100 inclusive (0 chance items never succeed!)
+        if (slotMove.defense_chance >= ranNum){
+            return slotMove.defense;
+        } else {
+            return 0;
+        }
+    }
+    function computerSlot(slots){
+        $("#actions").append(`${slots.length}`);
+        var ranNum = randomNumber(0, slots.length - 1);
+        $("#actions").append(`${ranNum}<br>`);
+        return this.slots[ranNum];
+    }
+
+
+
+    function madeAMove(playerSlot) {
+
+        $("#actions").append(`Player chose ${playerSlot.name}<br>`);
+        player_attack = attackValue(playerSlot);
+        player_defense = defenseValue(playerSlot);
+        
+        computerMove = computer.moveSet[randomNumber(0,1)];
+        $("#actions").append(`${computer.name} chose ${computerMove.name}<br>`);
+        computer_attack = attackValue(computerMove);
+        computer_defense = defenseValue(computerMove);
+
+        damageToComp = 0;
+        damageToPlayer = 0;
+        if (player_attack > computer_defense){
+            damageToComp = player_attack - computer_defense;
+        }
+        if (computer_attack > player_defense){
+            damageToPlayer = computer_attack - player_defense;
+        }
+
+
+        message = "";
+        if (player_attack == playerSlot.attack && player_attack > 0) {
+            message = message + `${playerSlot.name} was a hit! It caused ${player_attack}<br>`
+        } else if (player_attack != playerSlot.attack) {
+            message = message + `${playerSlot.name} Missed!<br>`
+        }
+
+        if (player_defense == playerSlot.defense && player_defense > 0) {
+            message = message + `${playerSlot.name} deflected ${player_defense} damage!<br>`
+        } else if (player_defense != playerSlot.defense) {
+            message = message + `${playerSlot.name} was too slow!<br>`
+        }
+
+        if (computer_attack == computerMove.attack && computer_attack > 0) {
+            message = message + `${computerMove.name} hit you with ${computer_attack}!<br>`
+        } else if (computer_attack != computerMove.attack) {
+            message = message + `${computerMove.name} missed!<br>`
+        }
+
+        if (computer_defense == computerMove.defense && computer_defense > 0) {
+            message = message + `${computerMove.name} deflected ${computer_defense} damage!<br>`
+        } else if (computer_defense != computerMove.defense) {
+            message = message + `${computerMove.name} was too slow!<br>`
+        }
+
+        message = message + `Total Damage given: ${damageToComp}<br>Total Damage taken: ${damageToPlayer}<br>`
+        
+        $("#actions").append(`${message}`)
+        changeHealth(player, damageToPlayer);
+        changeHealth(computer, damageToComp);
+        print_charachters();
+        checkBattleStatus();
+    }
+    function checkBattleStatus(){
         if (computer.health <= 0) {
             computerPicked = false;
             $("#computer-stats")
@@ -146,23 +312,36 @@
             }
             return false;
         }
-
-        // cpu attacks player
-
-        player.health -= computer.counter;
-
-        $("#player-stats").html(`<h4>${player.name} - Attack</h4>HP: ${player.health} <br/>Attack: ${player.attack}<br/>Counter: ${player.counter}`);
-
-        $("#computer-stats").html(`<h4>${computer.name} - Defender</h4>HP: ${computer.health} <br/> Attack: ${computer.attack}<br/>Counter: ${computer.counter}`);
-
         if (player.health <= 0) {
             playerPicked = false;
             computerPicked = false;
             gameRunning = false;
-            $("#player-stats").html(`<h3> Defeated!</h3><h4>${player.name} - Defender</h4>HP: ${player.health} <br/>Attack: ${player.attack}<br/>Counter: ${player.counter}`).addClass("defeated")
+            $("#player-stats")
+                .html(`<h3> Defeated!</h3><h4>${player.name} - Defender</h4>HP: ${player.health} <br/>Attack: ${player.attack}<br/>Counter: ${player.counter}`)
+                .addClass("defeated")
 
             alert("You've lost! Start new game!")
         }
+    }
+
+    function attack() {
+        //player attacks cpu
+        changeHealthAfterAttack(player, computer);
+        //player's attack increases
+        player.attack += player.defaultAttack;
+        $("#actions").append(`Player chose ${player.moveSet[0].name}<br>`)
+        // check to see if defender has lost
+        
+
+        // cpu attacks player
+        changeHealthAfterAttack(computer, player);
+        print_charachters(); 
+    }
+    function slot1() {
+        madeAMove(player.moveSet[0])
+    }
+    function slot2() {
+        madeAMove(player.moveSet[1])
     }
 
     //New Game Button
@@ -170,14 +349,25 @@
 
     // Attack button
 
-    $("#attack").on("click", function () {
+    $("#slot1").on("click", function () {
         if (playerPicked && computerPicked && gameRunning) {
-            attack();
+            slot1();
         }
         else {
             alert("You need to pick a player!")
         }
     })
+
+
+    $("#slot2").on("click", function () {
+        if (playerPicked && computerPicked && gameRunning) {
+            slot2();
+        }
+        else {
+            alert("You need to pick a player!")
+        }
+    })
+
     newGame();
     
 
